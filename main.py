df72c3f9a99f1d31301b7ed2b7793a7313828ebf
AshtonMarley <ashtonmarley@tutamail.com>
2025-12-09 01:55:29 -0800

    Completed UI and added requirements

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
 import os
+import sys
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QLabel, QVBoxLayout, QHBoxLayout,
-    QPushButton, QCheckBox, QFileDialog, QMessageBox
+    QPushButton, QCheckBox, QFileDialog, QMessageBox, QGridLayout
 )
 from PyQt6.QtCore import Qt
 
@@ -14,15 +15,32 @@ SETTINGS_FILE = "settings.json"
 class MainWindow(QWidget):
   def __init__(self):
     super().__init__()
-    self.title = "Settings Panel"
+    # define the positions for the settings box
+    self.title_row =          1
+    self.keyboard_watch_row = 2
+    self.mouse_watch_row =    3
+    self.screen_watch_row =   4
+    self.confirm_button_row = 5
+    self.cancel_button_row =  6
+    
+    self.default_col =        0
+    self.align_center =       Qt.AlignmentFlag.AlignCenter # position flag
+    
+
+    # default setup for window
+    self.title =         "Settings Panel"
+    self.window_width =  200
+    self.window_height = 200
+    
     self.setWindowTitle(self.title)
-    self.setGeometry(100, 100, 800, 800)
+    self.setFixedSize(self.window_width, self.window_height)
     
-    self.layout = QHBoxLayout()
-    self.setLayout(self.layout)
-    
-    
+    self.layout_main = QVBoxLayout()
+    self.setLayout(self.layout_main)
     self.settingsBox()
+    
+    
+
     
   def settingsBox(self):
     """
@@ -38,18 +56,42 @@ class MainWindow(QWidget):
     and then all the way at the bottom there will be a time delay for how often the program goes off
     
     """
-    # make the label
-    title = QLabel("Mouse Tracker", self)
+    layout_vertical = QVBoxLayout() # setup the layout for vertical
+    label_title =     QLabel("Settings Panel")
+    watch_keyboard =  QCheckBox("Keyboard Listen")
+    watch_mouse =     QCheckBox("Mouse Listen")
+    watch_screen =    QCheckBox("Screen Listen")
     
-    self.layout.addWidget(title)
+    layout_vertical.addWidget(label_title)
+    layout_vertical.addWidget(watch_keyboard)
+    layout_vertical.addWidget(watch_mouse)
+    layout_vertical.addWidget(watch_screen)
     
+    
+    layout_horizontal = QHBoxLayout()
+    button_confirm =    QPushButton("Apply")
+    button_cancel =     QPushButton("Cancel")
+    layout_horizontal.addWidget(button_confirm)
+    layout_horizontal.addWidget(button_cancel)
+
+    
+    self.layout_main.addLayout(layout_vertical)
+    self.layout_main.addLayout(layout_horizontal)
+    
+    # get the states of the checkboxes
+    checkbox_state_keyboard = watch_keyboard.checkState()
+    checkbox_state_mouse    = watch_mouse.checkState()
+    checkbox_state_screen   = watch_screen.checkState()
+  
+
     
 
 def main():
-    app = QApplication([])
-    w = MainWindow()
-    w.show()
-    app.exec()
+  os.system("clear")
+  app = QApplication([])
+  w = MainWindow()
+  w.show()
+  app.exec()
 
 
 if __name__ == "__main__":
--- process.py
+++ process.py
@@ -7,6 +7,7 @@ import time
 class compInputCapture:
   def __init__(self):
     self.TIME_delay = 0.01
+    self.state_mouse = False
     
   def capMouseVel(self):
     x1,y1 = pyautogui.position()
@@ -14,7 +15,17 @@ class compInputCapture:
     x2,y2 = pyautogui.position()
     xvel = abs(x2 - x1) / self.TIME_delay
     yvel = abs(y2 - y1) / self.TIME_delay
-    return xvel, yvel
+    
+    if abs(xvel + yvel) == 0:
+      self.state_mouse = True
+      return self.state_mouse
+    
+    self.state_mouse = False
+
+  
+  def keyboardCapture(self):
+    pass
+  
   
   
 def _runProc(permissions): # permissions should be a dictionary
